ddb9f3b638a3bc36690292d0e6692ee7e3202d55
Nielson Phu <nielson.phu@gmail.com>
2018-02-22 13:07:54 -0500

    Minor revisions to Home component.

--- client/test/components/pages/Home.spec.js
+++ client/test/components/pages/Home.spec.js
@@ -1,10 +1,9 @@
-/* Copyright 2017-2018, Frameshift Labs, Inc., All rights reserved. */
 import { shallow } from '@vue/test-utils';
 import Home from '../../../app/components/pages/Home.vue';
 
 describe('Home', () => {
 
-  it('shows a message telling the user to login first', () => {
+  it('sets a greeting', () => {
     let wrapper = shallow(Home);
     expect(wrapper.vm.greeting).to.equal('hello world!')
   });
